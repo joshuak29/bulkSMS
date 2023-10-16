f6ac675fb46d6c45cb12fcedd871c8b670d84aae
joshuak29 <rockjoshk29@gmail.com>
2023-10-16 10:43:39 +0200

    last commit

--- bulk sms/src/components/main/Header.vue
+++ bulk sms/src/components/main/Header.vue
@@ -2,7 +2,8 @@
     <div class="w-full h-16 px-4 py-1 bg-blue-500 text-white flex flex-row gap-4 items-center justify-end shadow-md">
         <!-- name and role -->
         <div class="flex flex-col h-full">
-            <div class="font-semibold">Rukundo Joshua  (  )</div>
+            <div class="font-semibold" v-if="authStore.user">{{ authStore.user.name}}  ( {{ authStore.user.username }} )</div>
+            <div class="font-semibold" v-else>Loading...</div>
             <div class="text-gray-300 text-xs text-right" v-if="authStore.isAdmin()">Admin</div>
             <div class="text-gray-300 text-xs text-right" v-else>Agent</div>
         </div>
@@ -12,14 +13,19 @@
     </div>
 </template>
 <script setup>
-import { computed } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useAuthStore } from '@/stores/authStore';
 
 const authStore = useAuthStore();
 
-const username = computed(() => {
-    return authStore.user.username
+onMounted(() => {
+    console.log(authStore.user)
 })
+const username = computed(() => {
+    return authStore.user
+})
+
+// {username}
 </script>
 <style scoped>
     
